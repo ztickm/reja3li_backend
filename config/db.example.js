6494370def991d33763872bf87e7567891582489
Salim Mahboubi <ztickm@gmail.com>
2018-05-11 21:39:33 +0100

    Added db.example.js

---
+++ config/db.example.js
@@ -0,0 +1,5 @@
+//Rename this file to db.js and replace the placeholder url by your MongoDB URI
+//We're using MongoClient as our driver
+module.exports = {
+    url: "mongodb://USERNAME_EXAMPLE:PASSWORD_EXAMPLE@example.example.com:0000/reja3li_app_db"
+}
