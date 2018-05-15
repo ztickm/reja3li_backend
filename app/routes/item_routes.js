b0fd2fe4ecc8bca6ac38ff60180a890fa5dc4b99
Salim Mahboubi <ztickm@gmail.com>
2018-05-15 23:15:27 +0100

    Changed route from lentelement to item +renamed note object to item + Added some fields to the item object

--- app/routes/item_routes.js
+++ app/routes/item_routes.js
@@ -2,20 +2,27 @@ let ObjectID = require('mongodb').ObjectID;
 
 module.exports = function (app, db) {
 
-    app.post('/lentelements', (req, res) => {
-        const note = {
-            name: req.body.name, description: req.body.description,
-            photoURL: req.body.photoURL, lender_id: req.body.lender_id,
-            borrower_id: req.body.borrower_id
+    app.post('/items', (req, res) => {
+        const item = {
+            name: req.body.name,
+            description: req.body.description,
+            type: req.body.type,
+            amount: req.body.amount,
+            owner_id: req.body.owner_id,
+            created_at: req.body.created_at,
+            updated_at: req.body.updated_at,
+            photoURL: req.body.photoURL,
+            lent_to_id: req.body.lender_id,
+            borrowed_from_id: req.body.borrower_id
         }
-        db.collection('elements').insert(note, (err, result) => {
+        db.collection('elements').insert(item, (err, result) => {
             if (err) res.send({ 'error': 'an errors has occured' + err })
             else res.send(result.ops[0])
 
         })
     });
 
-    app.get('/lentelements/:id', (req, res) => {
+    app.get('/items/:id', (req, res) => {
         const id = req.params.id;
         const details = { '_id': new ObjectID(id) };
         db.collection('elements').findOne(details, (err, item) => {
@@ -25,20 +32,28 @@ module.exports = function (app, db) {
     })
 
     //TODO: add some conditional logic to update the fields only if they’re present in the request
-    app.put('/lentelements/:id', (req, res) => {
+    app.put('/items/:id', (req, res) => {
         const id = req.params.id;
         const details = { '_id': new ObjectID(id) };
-        const note = {
-            name: req.body.name, description: req.body.description,
-            photoURL: req.body.photoURL, lender_id: req.body.lender_id,
-            borrower_id: req.body.borrower_id
+        const item = {
+            name: req.body.name,
+            description: req.body.description,
+            type: req.body.type,
+            amount: req.body.amount,
+            owner_id: req.body.owner_id,
+            created_at: req.body.created_at,
+            updated_at: req.body.updated_at,
+            photoURL: req.body.photoURL,
+            lent_to_id: req.body.lender_id,
+            borrowed_from_id: req.body.borrower_id
         }
-        db.collection('elements').update(details, note, (err, item) => {
+        db.collection('elements').update(details, item, (err, item) => {
             if (err) res.send({ 'error': 'an errors has occured' + err });
             else res.send(item);
         })
     })
-    app.delete('/lentelements/:id', (req, res) => {
+
+    app.delete('/items/:id', (req, res) => {
         const id = req.params.id;
         const details = { '_id': new ObjectID(id) };
         db.collection('elements').remove(details, (err, item) => {
